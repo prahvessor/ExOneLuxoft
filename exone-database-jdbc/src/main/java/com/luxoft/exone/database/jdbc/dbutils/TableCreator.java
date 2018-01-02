44829fa59eaa02604a0310ea9aebef65b1dcd7f0
Emin K. Aliiev <emin.aliev@gmail.com>
2018-01-02 22:39:20 +0200

    Added compatibility to TaskTwo. Now TaskOne and TaskTwo uses the same tables in DB

--- exone-database-jdbc/src/main/java/com/luxoft/exone/database/jdbc/dbutils/TableCreator.java
+++ exone-database-jdbc/src/main/java/com/luxoft/exone/database/jdbc/dbutils/TableCreator.java
@@ -8,27 +8,30 @@ public class TableCreator {
 
     private static void createTable() {
 
-        final String SQL_LINE = "DROP TABLE IF EXISTS LINESTATISTIC;" +
-                                "CREATE TABLE LINESTATISTIC(ID_LINE INT AUTO_INCREMENT,\r\n" + 
-                                                            "PRIMARY KEY (ID_LINE),\r\n" + 
-                                                            "LONGEST_WORD INT," +
-                                                            "SHORTEST_WORD INT," +
-                                                            "AVG_WORD INT," +
-                                                            "LINE_LENGTH INT)";
-        final String SQL_FILE = "DROP TABLE IF EXISTS FILESTATISTIC;\r\n" + 
-                                "CREATE TABLE FILESTATISTIC(ID_FILE INT AUTO_INCREMENT,\r\n" + 
+        final String SQL_FILE = "DROP TABLE IF EXISTS FILE_STATISTIC;\r\n" + 
+                                "CREATE TABLE FILE_STATISTIC(ID_FILE INT AUTO_INCREMENT,\r\n" + 
                                                             "PRIMARY KEY (ID_FILE),\r\n" + 
                                                             "LONGEST_FILE_WORD INT," +
                                                             "SHORTEST_FILE_WORD INT," +
                                                             "AVG_FILE_WORD INT," +
                                                             "AVG_LINE_LENGTH INT)";
+        final String SQL_LINE = "DROP TABLE IF EXISTS LINE_STATISTIC;" +
+                                "CREATE TABLE LINE_STATISTIC(ID_LINE INT AUTO_INCREMENT,\r\n" + 
+                                                            "PRIMARY KEY (ID_LINE),\r\n" +
+                                                            "ID_FILE INT," + 
+                                                            "LONGEST_WORD INT," +
+                                                            "SHORTEST_WORD INT," +
+                                                            "AVG_WORD INT," +
+                                                            "LINE_LENGTH INT, " +
+                                                            "FOREIGN KEY (ID_FILE) REFERENCES FILE_STATISTIC (ID_FILE))";
 
         try (Connection con = DBConnector.getConnection();
-             Statement stmtLine = con.createStatement();
              Statement stmtFile = con.createStatement()) {
-
-            stmtLine.executeUpdate(SQL_LINE);
-            stmtFile.executeUpdate(SQL_FILE);
+             Statement stmtLine = con.createStatement();
+             
+             stmtFile.executeUpdate(SQL_FILE);
+             stmtLine.executeUpdate(SQL_LINE);
+            
 
         }catch (SQLException e) {
             e.printStackTrace();
--- exone-database-jdbc/src/main/java/com/luxoft/exone/database/jdbc/dbutils/TableUpdater.java
+++ exone-database-jdbc/src/main/java/com/luxoft/exone/database/jdbc/dbutils/TableUpdater.java
@@ -5,48 +5,48 @@ import com.luxoft.exone.database.jdbc.statisticbeans.LineStatistic;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.List;
 
 public class TableUpdater {
 
     public void updateTable(List<FileStatistic> fileStatisticList) {
 
-        String sqlLine ="INSERT INTO LINESTATISTIC(LONGEST_WORD, SHORTEST_WORD, AVG_WORD, LINE_LENGTH) VALUES(?,?,?,?)";
-        String sqlFile ="INSERT INTO FILESTATISTIC(LONGEST_FILE_WORD, SHORTEST_FILE_WORD, AVG_FILE_WORD, AVG_LINE_LENGTH) VALUES(?,?,?,?)";
+        String sqlLine ="INSERT INTO LINE_STATISTIC(ID_FILE, LONGEST_WORD, SHORTEST_WORD, AVG_WORD, LINE_LENGTH) VALUES(?,?,?,?,?)";
+        String sqlFile ="INSERT INTO FILE_STATISTIC(LONGEST_FILE_WORD, SHORTEST_FILE_WORD, AVG_FILE_WORD, AVG_LINE_LENGTH) VALUES(?,?,?,?)";
 
         try (Connection connection = DBConnector.getConnection();
              PreparedStatement prStatementLine = connection.prepareStatement(sqlLine);
-             PreparedStatement prStatementFile = connection.prepareStatement(sqlFile)) {
+             PreparedStatement prStatementFile = connection.prepareStatement(sqlFile);
+             Statement statementId = connection.createStatement();) {
             
             for (FileStatistic fileStatistic : fileStatisticList) {
                 
-                updateTable(prStatementFile,
-                        fileStatistic.getLongestFileWord(),
-                        fileStatistic.getShortestFileWord(),
-                        fileStatistic.getAvgFileWord(),
-                        fileStatistic.getAvgFileLineLength());
+                prStatementFile.setInt(1, fileStatistic.getLongestFileWord());
+                prStatementFile.setInt(2, fileStatistic.getShortestFileWord());
+                prStatementFile.setInt(3, fileStatistic.getAvgFileWord());
+                prStatementFile.setInt(4, fileStatistic.getAvgFileLineLength());
+                prStatementFile.executeUpdate();
+                
+                ResultSet rs = statementId.executeQuery("SELECT LAST_INSERT_ID() AS ID_FILE");
+                int idFile = 0;
+                while (rs.next()) {
+                    idFile = rs.getInt("id_file");
+                }
                 
                 for (LineStatistic lineStatistic : fileStatistic.getLineStatisticList()) {
-                    
-                    updateTable(prStatementLine,
-                            lineStatistic.getLongestWord(),
-                            lineStatistic.getShortestWord(),
-                            lineStatistic.getAvgWord(),
-                            lineStatistic.getLineLength());
+                  
+                    prStatementLine.setInt(1, idFile);
+                    prStatementLine.setInt(2, lineStatistic.getLongestWord());
+                    prStatementLine.setInt(3, lineStatistic.getShortestWord());
+                    prStatementLine.setInt(4, lineStatistic.getAvgWord());
+                    prStatementLine.setInt(5, lineStatistic.getLineLength());
+                    prStatementLine.executeUpdate();
                 }
             }
         }catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private void updateTable(PreparedStatement prStatement, int longest, int shortest, int avg, int line) throws Exception{
-
-        prStatement.setInt(1, longest);
-        prStatement.setInt(2, shortest);
-        prStatement.setInt(3, avg);
-        prStatement.setInt(4, line);
-
-        prStatement.executeUpdate();
     }
 }
